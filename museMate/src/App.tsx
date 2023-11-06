35a1e068d6c1ce26ebcb072199e095f8385b0f7a
Danilka-Akarawita <danilka.20210514@iit.ac.lk>
2023-11-06 22:48:15 +0530

    add responsivitiy

--- museMate/src/App.tsx
+++ museMate/src/App.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import Navbar from './components/Navbar';
 import { SELECTEDPAGE } from './types/selectedPageEnums';
@@ -8,10 +8,23 @@ import { SELECTEDPAGE } from './types/selectedPageEnums';
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SELECTEDPAGE>(SELECTEDPAGE.HOME );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SELECTEDPAGE.HOME);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <>
-      <Navbar  selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <Navbar  isTopOfPage={true} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
 
     </>
   
